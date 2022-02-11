74a9f7b57d9aa12d53c0727131b29a4932b50858
angelhumberto99 <andre.gutierrez99@gmail.com>
2022-02-10 20:54:42 -0600

    Instructions added to render

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def main(argv):
     fig = Get_Figure_Vertices(argv)
 
     projection = Projection(screen, width, height, fig)
+
+    pygame.font.init()
+    text = pygame.font.SysFont('Comic Sans MS', 20)
+    msg = "Use arrow keys to interact with X and Y rotations"
+    msg2 = "Hold Spacebar + arrow keys to rotate Z axis"
+    msg3 = "Use the scroll wheel to scale the figure"
+    txt_surf = text.render(msg, False, "white")
+    txt_surf2 = text.render(msg2, False, "white")
+    txt_surf3 = text.render(msg3, False, "white")
     
     while True:
         for event in pygame.event.get():
@@ -31,6 +40,9 @@ def main(argv):
                 projection.scale += event.y
 
         screen.fill("black") 
+        screen.blit(txt_surf, (20, 10))
+        screen.blit(txt_surf2, (20, 35))
+        screen.blit(txt_surf3, (20, 60))
         projection.Update()
         pygame.display.update()
         clock.tick(60)
